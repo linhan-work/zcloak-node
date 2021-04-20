5b4047d40982d3740ad4ae88c31680c8195b7681
Dejavukong <foxcong.yolo@gmail.com>
2021-04-20 16:35:12 +0800

    add more debug poing

--- primitives/starks/src/lib.rs
+++ primitives/starks/src/lib.rs
@@ -1,8 +1,8 @@
+
 #![warn(missing_docs)]
 
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(not(feature = "std"), feature(alloc_error_handler))]
-
 
 #![cfg_attr(feature = "std",
    doc = "Substrate runtime standard library as compiled when linked with Rust's standard library.")]
@@ -10,10 +10,18 @@
    doc = "Substrate's runtime standard library as compiled without Rust's standard library.")]
 
 
-use sp_runtime_interface::{runtime_interface, Pointer};
-
+use sp_runtime_interface::{runtime_interface, Pointer,
+	pass_by::{PassBy, PassByCodec}
+};
+use codec::{Encode, Decode};
 #[cfg(feature = "std")]
 use distaff::StarkProof;
+
+#[derive(PassByCodec, Encode, Decode)]
+pub enum VerifyErr {
+	SerializeErr,
+	DistaffVerifyErr
+}
 
 #[runtime_interface]
 pub trait Starks {
@@ -22,9 +30,15 @@ pub trait Starks {
 		program_hash: &[u8; 32],
 		public_inputs: &[u128],
 		outputs: &[u128],
-		proof: &[u8]) -> Result<bool, ()>
+		proof: &[u8]) -> Result<bool, VerifyErr>
 	{
-		let stark_proof = bincode::deserialize::<StarkProof>(&proof).unwrap();
-		distaff::verify(program_hash, public_inputs, outputs, &stark_proof).map_err(|_e| ())
+		log::info!(target: "starks-verifier", "@@@@@@@ Start to stark verify");
+		let stark_proof = bincode::deserialize::<StarkProof>(&proof).map_err(|_| VerifyErr::SerializeErr)?;
+		log::debug!(target: "starks-verifier", "$$$$$$ StarkProof Parsing is ok");
+		let res = distaff::verify(program_hash, public_inputs, outputs, &stark_proof);
+		
+		log::debug!(target: "starks-verifier", "@@##$$@@##$$@@##$$ THE RES IS {:?}", res);
+		
+		return Err(VerifyErr::DistaffVerifyErr)
 	}
 }
